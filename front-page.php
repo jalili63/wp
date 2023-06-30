dee849d749c33c99df1ca1e6f2048cb8b337171e
jalili63 <jalili63@outlook.com>
2023-06-30 18:19:46 +0330

    first commit

---
+++ front-page.php
@@ -0,0 +1,10 @@
+<?php get_header() ?>
+
+<h1>index page</h1>
+<?php get_template_part('partials/index','slider') ?>
+<?php get_template_part('partials/index','content') ?>
+<?php //get_query_var('var','text');
+is_front_page()
+
+ ?>
+<?php get_footer() ?>
---
+++ functions.php
@@ -0,0 +1,6 @@
+<?php
+
+//function custom_js(){
+//	echo '<script>alert("Reza")</script>';
+//}
+//add_action('wp_footer','custom_js');
